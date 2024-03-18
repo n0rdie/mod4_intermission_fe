c93e3579ac3588328e40853408ab740dcb0195c3
Quin Nordmark <quinnordmark@gmail.com>
2024-03-18 03:38:59 -0600

    search done

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
 class NewsController < ApplicationController
     def index
-        @input = params[:input]
-		@news = NewsFacade.search(params[:input])
+        @input = params[:q]
+		@news = NewsFacade.search(@input)
     end
 end
